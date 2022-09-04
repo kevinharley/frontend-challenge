b914c490e75393a49074a4f43d3de92d69195746
kevinharley <harley.kj@gmail.com>
2022-09-04 10:27:08 -0300

    fixing disabled state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Button = ({ children, onClick, type, className, ...props }) => {
   return (
     <button
-      className={`border-2 border-green-500 bg-green-500 hover:bg-green-700 hover:border-green-700 text-white  p-2 mr-2 disabled:opacity-50  ${className}`}
+      className={`border-2 border-green-500 bg-green-500 hover:bg-green-700 hover:border-green-700 text-white  p-2 mr-2 disabled:opacity-50 disabled:hover:bg-green-500 disabled:border-green-500 ${className}`}
       type={type}
       onClick={onClick}
       {...props}
